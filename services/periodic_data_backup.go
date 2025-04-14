d9ad130f4af4e147eafddd2d132a45f638de5e49
Mauricio Curiel <mauriciom.curiel@gmail.com>
2025-04-13 17:37:55 -0700

    move data collection to backupDataToFirestore to ensure new values are retrieved. Also added window data and an improved time for ML trainig. Lastly, added error handling and logging to continue server dispite temporary errors without loosing data.

--- services/periodic_data_backup.go
+++ services/periodic_data_backup.go
@@ -45,27 +45,47 @@ func backupDataToFirestore(client *firestore.Client, data map[string]interface{}
 	log.Println("Data backed up successfully to Firestore")
 }
 
-// Launch go routine to backup data every x seconds
-func Interval_backup(seconds int) {
-	client := initFirebase()
-	defer client.Close()
-
+func compileData() (map[string]interface{}, error[]) {
 	indoor_temp, err := ReadTemperature()
+	errors := make([]error, 0)
 	if err != nil {
-		log.Fatalln("Error reading indoor temperature: ", err)
+		log.Println("Error reading indoor temperature: ", err)
+		errors = append(errors, err)
 	}
 	outdoor_temp, err := FetchOutdoorTemperature()
 	if err != nil {
-		log.Fatalln("Error fetching outdoor weather: ", err)
+		log.Println("Error fetching outdoor weather: ", err)
+		errors = append(errors, err)
+	}
+	window_open, is_window_event, err := IsWindowOpen()
+	if err != nil {
+		log.Println("Error checking window status: ", err)
+		errors = append(errors, err)
 	}
 
-	// Example data to backup
+	// Convert time to minuets since Jan 1
+	time_now := time.Now()
+	// Round down to the nearest minute
+	time_now = time.Date(time_now.Year(), time_now.Month(), time_now.Day(), time_now.Hour(), time_now.Minute(), 0, 0, time.UTC)
+	time_diff := time_now.Sub(time.Date(time_now.Year(), 1, 1, 0, 0, 0, 0, time.UTC))
+	minutes_of_year := int(time_diff.Minutes())
+
 	data := map[string]interface{}{
 		"indoor-temp":  indoor_temp,
 		"outdoor-temp": outdoor_temp,
 		// "humidity":    60,
-		"time": time.Now(),
+		"time":         minutes_of_year,
+		"window-open":  window_open,
+		"window-event": is_window_event,
 	}
+
+	return data, errors
+}
+
+// Launch go routine to backup data every x seconds
+func Interval_backup(seconds int) {
+	client := initFirebase()
+	defer client.Close()
 
 	ticker := time.NewTicker(time.Duration(seconds) * time.Second)
 	defer ticker.Stop()
@@ -73,7 +93,14 @@ func Interval_backup(seconds int) {
 	for {
 		select {
 		case <-ticker.C:
+			data, errors := compileData()
+			if len(errors) > 0 {
+				log.Println("Errors occurred during data compilation: ", errors)
+				log.Println("Skipping backup due to errors. The data may be incomplete.\nDATA:", data)
+				continue
+			}
 			backupDataToFirestore(client, data)
+			log.Println("Data backed up successfully to Firestore")
 		}
 	}
 }
